e298b90b69e7228c027ac5fcf469150bc0267da9
Martin Staadecker <machstg@gmail.com>
2024-10-08 17:31:54 -0400

    Disable SSR and add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
     import "mathlive/fonts.css" // Ensures fonts are imported
-
+    import { MathfieldElement } from "mathlive";
     import { onMount } from "svelte";
 
     let mathField: HTMLDivElement;
 
     onMount(async () => {
-        // We need to do a dynamic import to avoid MathField being defined after it becomes available? Is this because npm run dev is doing SSR?
-        const { MathfieldElement } = await import("mathlive");
         const mfe = new MathfieldElement();
         mfe.value = 'f(x) = \\sin(x+\\pi)';
         mathField.appendChild(mfe);
     });
 </script>
 
-<h1>PrettyEQ</h1>
-<div bind:this={mathField}></div>
+<div class="grid h-screen place-items-center">
+    <h1>PrettyEQ</h1>
+
+
+    <div bind:this={mathField}></div>
+
+</div>
