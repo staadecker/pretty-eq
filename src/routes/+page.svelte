3a032352d6a6c99ceaade4251a2f2858e052ad8a
Martin Staadecker <machstg@gmail.com>
2024-10-08 17:23:52 -0400

    Get equations working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import "mathlive/fonts.css" // Ensures fonts are imported
+
+    import { onMount } from "svelte";
+
+    let mathField: HTMLDivElement;
+
+    onMount(async () => {
+        // We need to do a dynamic import to avoid MathField being defined after it becomes available? Is this because npm run dev is doing SSR?
+        const { MathfieldElement } = await import("mathlive");
+        const mfe = new MathfieldElement();
+        mfe.value = 'f(x) = \\sin(x+\\pi)';
+        mathField.appendChild(mfe);
+    });
+</script>
+
+<h1>PrettyEQ</h1>
+<div bind:this={mathField}></div>
